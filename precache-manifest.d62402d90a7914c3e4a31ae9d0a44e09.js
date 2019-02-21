dc79133167b0602b36790086f0bc60c73f7f9b8e
Ebru Gulec <ebru@ebrugulec.com>
2019-02-21 15:31:11 +0300

    Updates

---
+++ precache-manifest.d62402d90a7914c3e4a31ae9d0a44e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3689b3f77e87a07617c8",
+    "url": "/text-animation-hooks/static/js/runtime~main.3689b3f7.js"
+  },
+  {
+    "revision": "97315bcbd96c4b2fcc94",
+    "url": "/text-animation-hooks/static/js/main.97315bcb.chunk.js"
+  },
+  {
+    "revision": "888a8d50edc36005de71",
+    "url": "/text-animation-hooks/static/js/3.888a8d50.chunk.js"
+  },
+  {
+    "revision": "54094b48b410ee26b80e",
+    "url": "/text-animation-hooks/static/js/2.54094b48.chunk.js"
+  },
+  {
+    "revision": "97315bcbd96c4b2fcc94",
+    "url": "/text-animation-hooks/static/css/main.6238c7be.chunk.css"
+  },
+  {
+    "revision": "1d870b41d73e7e163611bc69e281bec7",
+    "url": "/text-animation-hooks/index.html"
+  }
+];
